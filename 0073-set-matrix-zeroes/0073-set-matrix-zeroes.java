6de1fbb29c8d2af25bb10c9ddf117ba55c6a832d
Partha02 <116193655+Partha02Bh@users.noreply.github.com>
2025-05-21 11:13:44 +0530

    Time: 1 ms (72.83%), Space: 45.7 MB (66.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,26 +1,35 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int m = matrix.length, n = matrix[0].length;
+        boolean row0 = false, col0 = false;
 
-        int row[] = new int[m];
-        int col[] = new int[n];
+        // Check if first row or first column need to be zero
+        for (int i = 0; i < m; i++) if (matrix[i][0] == 0) col0 = true;
+        for (int j = 0; j < n; j++) if (matrix[0][j] == 0) row0 = true;
 
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(matrix[i][j]==0) {
-                    row[i]=1;
-                    col[j]=1;
+        // Mark cells in first row and column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(row[i]==1 || col[j]==1) {
-                    matrix[i][j]=0;
-                }
+        // Set cells to zero using the markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
             }
         }
+
+        // Handle first column
+        if (col0) for (int i = 0; i < m; i++) matrix[i][0] = 0;
+
+        // Handle first row
+        if (row0) for (int j = 0; j < n; j++) matrix[0][j] = 0;
+
     }
 }
